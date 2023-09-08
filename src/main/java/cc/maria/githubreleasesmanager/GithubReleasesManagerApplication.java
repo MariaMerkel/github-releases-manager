26b5b7735ce2e0a76ca454f8a9a2862ff6debac5
Maria Merkel <maria@merkel.name>
2023-09-08 16:45:02 +0200

    Initial commit

---
+++ src/main/java/cc/maria/githubreleasesmanager/GithubReleasesManagerApplication.java
@@ -0,0 +1,13 @@
+package cc.maria.githubreleasesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubReleasesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubReleasesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/maria/githubreleasesmanager/GithubReleasesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.maria.githubreleasesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubReleasesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
