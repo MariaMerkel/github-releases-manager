e663f8a195e97f1c7ff9b7c4b16b61e7e75c8754
Maria Merkel <maria@merkel.name>
2023-09-08 19:00:58 +0200

    Added TeamCity token

--- src/main/java/cc/maria/githubreleasesmanager/WebhookService.java
+++ src/main/java/cc/maria/githubreleasesmanager/WebhookService.java
@@ -1,9 +1,14 @@
 package cc.maria.githubreleasesmanager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @Service
 public class WebhookService {
@@ -19,7 +24,9 @@ public class WebhookService {
             GitHubReleaseResponse response = restTemplate.getForObject("https://api.github.com/repos/" + repository.getRepo() + "/releases/latest", GitHubReleaseResponse.class);
             if (Integer.toString(response.id).equals(repository.getLastRelease())) continue;
 
-            restTemplate.postForObject(repository.getWebhook(), null, Void.class);
+            MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+            headers.add("Authorization", "Bearer eyJ0eXAiOiAiVENWMiJ9.VlNuX1lTX0VYVURiMHpGbTNXSnZ5bGFVQm1B.ZmQ2NWQ2NTctMjNjNC00MWMzLWJiZTgtNjczYzVlZmRiOTAx");
+            restTemplate.postForObject(repository.getWebhook(), new HttpEntity<>("", headers), Void.class);
             repository.setLastRelease(Integer.toString(response.id));
             repositoriesRepository.save(repository);
         }
